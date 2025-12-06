30daa482fd7dc313213110421388ec7d0224ce61
Christan Sieukam <christansieukam@gmail.com>
2025-12-06 14:01:16 +0100

    Suppression de l'import inutile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ThemeProvider from "../components/theme-provider";
 import { Navigation } from "../components/navigation";
-import "../styles/navigation.styles.ts";
 
 const geist = Geist({ subsets: ["latin"] });
 const geistMono = Geist_Mono({ subsets: ["latin"] });
